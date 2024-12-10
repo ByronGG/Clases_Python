51511cafa370e00a5350d82ddebc456eb9bb45a0
ByronGG <anrushgg@gmail.com>
2024-12-10 10:49:02 -0600

    Remove unused test.py file

--- test.py
+++
@@ -1,6 +0,0 @@
-"""
-Dada una secuencia de N dígitos del 1 al 9, donde cada dígito puede aparecer con signo o, 
-escribe un programa que determine qué dígito apareció más veces 
-(independientemente de los signos) y cuántas veces apareció en su forma positiva y negativa.
-"""
-
