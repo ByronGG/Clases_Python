7a6904d90d7a022507dd2d98821832438436343a
ByronGG <anrushgg@gmail.com>
2025-05-17 11:13:09 -0600

    Prueba05

---
+++ Practicas/Practica05.py
@@ -0,0 +1,30 @@
+"""
+Crea una aplicación en Python que gestione un inventario de productos electrónicos (por ejemplo, celulares, laptops, tablets). Toda la información se almacenará en un archivo JSON. El programa debe permitir al usuario realizar las siguientes operaciones:
+
+    -Crear producto: Permite agregar un nuevo producto al inventario.
+    -Leer productos: Permite listar todos los productos o buscar por nombre, marca o categoría.
+    -Actualizar producto: Permite modificar los datos de un producto existente.
+    -Eliminar producto: Permite eliminar un producto del inventario.
+
+    Requisitos:
+
+Cada producto debe tener al menos estos campos:
+
+    id (numérico, único)
+    nombre (string)
+    marca (string)
+    categoria (string: “Celular”, “Laptop”, “Tablet”)
+    precio (float > 0)
+    stock (entero >= 0)
+
+El archivo JSON se llamará inventario.json.
+Implementa validaciones de entrada para que:
+
+    No se ingresen datos vacíos o no válidos.
+    El precio sea mayor a 0.
+    El stock no sea negativo.
+    La categoría solo pueda ser una de las permitidas.
+
+Cada operación CRUD debe confirmarse mostrando el resultado.
+El sistema debe permitir realizar varias operaciones hasta que el usuario decida salir.
+"""
