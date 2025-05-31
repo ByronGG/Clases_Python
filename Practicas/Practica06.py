875492af8395cdde1efd763299313e5b3b6c8141
ByronGG <anrushgg@gmail.com>
2025-05-31 12:51:10 -0600

    esqueleto de la tarea

--- Practicas/Practica06.py
+++ Practicas/Practica06.py
@@ -24,3 +24,57 @@ Requisitos:
     * El programa debe ser claro, útil y demostrar organización.
     * Bonus: Muestra los resultados en forma tabulada (puedes usar la librería estándar tabulate).
 """
+
+import json
+
+def cargar_ventas():
+    try:
+        with open("ventas.json", "r", encoding="utf-8") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print("No se encontró el archivo ventas.json")
+        return []
+
+def total_ventas_por_dia(ventas):
+    pass
+
+def producto_mas_vendido(ventas):
+    pass
+
+def clientes_frecuentes(ventas):
+    pass
+
+def buscar_ventas_por_fecha(ventas):
+    pass
+
+def menu():
+    ventas = cargar_ventas()
+    if not ventas:
+        print("No hay datos de ventas.")
+        return
+    
+    while True:
+        print("\n--- Analizador de Ventas Semanales ---")
+        print("1. Total de ventas por día")
+        print("2. Producto más vendido de la semana")
+        print("3. Clientes frecuentes (más de 2 por semana)")
+        print("4. Buscar ventas por fecha")
+        print("5. Salir")
+
+        opcion = input("Seleccione una opción (1 - 5): ").strip()
+        if opcion == "1":
+            total_ventas_por_dia(ventas)
+        elif opcion == "2":
+            producto_mas_vendido(ventas)
+        elif opcion == "3":
+            clientes_frecuentes(ventas)
+        elif opcion == "4":
+            buscar_ventas_por_fecha(ventas)
+        elif opcion == "5":
+            print("¡Hasta Luego!")
+            break
+        else:
+            print("Opción invalida")
+
+if __name__ == "__main__":
+    menu()
