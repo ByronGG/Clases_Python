c456a18d347a469b9a81e061b0d34df67cfbd0a2
Luis Alardin <anrushgg@gmail.com>
2026-04-18 14:42:26 -0600

    clase04pt1

---
+++ Clases_SQL/Clase04.sql
@@ -0,0 +1,75 @@
+/*
+    DML: Data Manipulation Language es el conjunto de comandos SQL que se usan para manipular los datos dentro de la tabla
+    Para que sirve:
+        - Insertar Datos
+        - Modificar Datos
+        - Eliminar Datos
+
+        INSERT UPDATE DELETE
+*/
+
+--INSERT Sirve para agregar registros a una tabla
+-- Inserta un solo valor
+INSERT INTO clientes(id, nombre, edad)
+VALUES (1, 'Arturo', 20);
+
+-- Inserta múltiples filas
+INSERT INTO clientes(id, nombre, edad)
+VALUES
+(2, 'Luis', 28),
+(3, 'Pedo', 21);
+
+-- Insertar sin especificar columnas (NO RECOMENDADO)
+INSERT INTO clientes
+VALUES (4, 'Maria', 30);
+
+-- UPDATE (Actualizar datos) Sirve para modificar registros existentes
+-- Acutaliza 1 solo datos
+UPDATE clientes
+SET edad = 19
+WHERE id = 1;
+
+-- Actualizar múltiples columnas
+UPDATE clientes
+SET nombre = 'Luis Fernando'
+    edad = 29
+WHERE id = 2;
+
+-- ERROR CRITICO
+UPDATE clientes
+SET edad = 50;
+-- REGLA DE ORO SIEMPRE USAR WHERE!!!!
+
+-- DELETE: Sirve para borrar registros
+-- Eliminar un registro especifico
+DELETE FROM clientes
+WHERE id = 1;
+
+-- ERROR CRITICO
+DELETE FROM clientes;
+
+-- DML y transacciones
+BEGIN;
+
+UPDATE clientes SET saldo = saldo - 100
+WHERE id = 1;
+UPDATE clientes SET saldo = saldo + 100
+WHERE id = 2;
+
+COMMIT;
+
+-- DML y DDL respeta la reglas este ultimo (DDL)
+-- EJEMPLO DE PARTE DEL DDL
+
+edad INT CHECK (edad >= 18)
+
+-- Cambio de valores por DML
+-- Te piden hacer un insert a la BD que ya tiene hasta ahora 550 registros y agregar al usuario 'Aruturo' a la tabla clientes
+-- La tabla clientes tiene 3 columnas id, nombre y edad
+INSERT INTO clientes VALUES(551, 'Arturo', 15); -- Este INSERT va a fallar
+
+CREATE TABLE clientes(
+    id INT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    edad INT CHECK (edad >= 18)
+);
